de0e03852bda524350b9fb7f070a477c398cd96a
Ildar Yulanov <yulanov.ildar@gmail.com>
2015-12-18 01:56:17 +0500

    Add snow

--- lib/christmas_tree_formatter/christmas_tree.rb
+++ lib/christmas_tree_formatter/christmas_tree.rb
@@ -28,15 +28,28 @@ class ChristmasTreeFormatter::ChristmasTree
 
   def print(symbol, *escapes)
     if @column == 0 and @column != start_column_for_row
-      output " " until @column == start_column_for_row
+      output snow(@column, :before) until @column == start_column_for_row
     end
 
     output escape(symbol, *escapes)
-    output "\n" if @column >= end_column_for_row
+    if @column >= end_column_for_row
+      snow(start_column_for_row, :after)
+      output "\n"
+    end 
   end
 
   def star
     print '⭒', :yellow, :blink
+  end
+
+  def snow(column = @column, position)
+    parity = position == :before ? (@row + 1) % 2 : 1
+
+    output_string = (' ' * column).split('').map.with_index do |symbol, index|
+      index % 2 == parity ? escape('*', :white) : symbol
+    end
+
+    @output.print output_string.join
   end
 
   def finalize
