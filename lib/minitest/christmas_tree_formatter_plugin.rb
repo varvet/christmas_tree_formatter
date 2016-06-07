12260e347fd95ac9789abf68f3f1d8855da07d68
Chris McKnight <cmckni3@gmail.com>
2016-06-07 15:51:35 -0500

    Adds minitest formatter
    closes #8

---
+++ lib/minitest/christmas_tree_formatter_plugin.rb
@@ -0,0 +1,58 @@
+require "christmas_tree_formatter/christmas_tree"
+
+module Minitest
+  def self.plugin_christmas_tree_formatter_init(options)
+    Minitest.reporter.reporters.clear
+    self.reporter << ChristmasTreeFormatter.new(options[:io], options.merge(total_count: total_count(options)))
+  end
+
+  def self.plugin_christmas_tree_formatter_options(opts, options)
+  end
+
+  class ChristmasTreeFormatter < Minitest::SummaryReporter
+    def start
+      super
+
+      max_width = 80
+      @width = 1
+      @width += 2 until (((@width / 2) * @width) / 2) >= total_count || @width >= max_width
+
+      @tree = ::ChristmasTreeFormatter::ChristmasTree.new(@width, output: io)
+      @tree.star
+    end
+
+    def record(result)
+      super
+
+      if result.skipped?
+        @tree.print "•", :yellow, :blink
+      elsif result.passed?
+        @tree.print "•", :green
+      else
+        @tree.print "•", :red
+      end
+    end
+
+    def report
+      @tree.finalize
+
+      super
+    end
+
+    def total_count
+      options[:total_count]
+    end
+  end
+
+  private
+
+  # stolen from minitest self.run
+  def self.total_count(options)
+    filter = options[:filter] || '/./'
+    filter = Regexp.new $1 if filter =~ /\/(.*)\//
+
+    Minitest::Runnable.runnables.map(&:runnable_methods).flatten.find_all { |m|
+      filter === m || filter === "#{self}##{m}"
+    }.size
+  end
+end
